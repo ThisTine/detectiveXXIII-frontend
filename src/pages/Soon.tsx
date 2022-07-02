9f3a2bfc91cab632d38cb067810f2aeb6bd0082e
I_AM_TINE <sutichok46@gmail.com>
2022-07-02 20:03:14 +0700

    add stay tune page

---
+++ src/pages/Soon.tsx
@@ -0,0 +1,18 @@
+import { Box, Container, Heading, Stack,Text } from '@chakra-ui/react'
+import Lottie from "lottie-react";
+import soonAnimation from '../animation/soon.json'
+const Soon = () => {
+  return (
+    <Stack justifyContent={"center"} alignItems="center" w="100%" h="100vh">
+        <Box padding={"50px"} bg="whiteAlpha.800" w="100%" rounded={"lg"} border="1px solid" borderColor={"gray.300"} backdropFilter="blur(20px)"  >
+        <Heading textAlign={"center"} color="gray.800" size={"2xl"} >STAY TUNED</Heading>
+        <Text textAlign={"center"} >We are trying our best to develop this website !</Text>
+            <Container w="100%" maxW="sm" >
+            <Lottie animationData={soonAnimation} loop={true}  />
+            </Container>
+        </Box>
+    </Stack>
+  )
+}
+
+export default Soon
