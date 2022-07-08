1980612b605560d479c833aaa7d5d58429254c86
sora-k46 <sorrawit456789@gmail.com>
2022-07-08 21:29:28 +0700

    Reset code

--- src/components/BoxContainer.tsx
+++ src/components/BoxContainer.tsx
@@ -1,10 +1,9 @@
 import { Box, Button, Container, VStack } from '@chakra-ui/react'
 import { FC, ReactNode } from 'react'
-import MissionBox from './MissionBox'
 
 
 
-const BoxContainer:FC<{children:ReactNode,Button?:ReactNode,MissionBox?:ReactNode}> = ({children,Button,MissionBox}) => {
+const BoxContainer:FC<{children:ReactNode,Button?:ReactNode}> = ({children,Button}) => {
   return (
     <Box border="1px solid"
      borderColor={"white"}
@@ -12,7 +11,6 @@ const BoxContainer:FC<{children:ReactNode,Button?:ReactNode,MissionBox?:ReactNod
       bg="#EBDCFC"
        w="100%" rounded={"3xl"}  display="flex" justifyContent={"center"} p={5}>
       <Container maxW="lg" >
-        {MissionBox && <VStack transform={'translateY(-100%)'} >{MissionBox}</VStack>}
         {children}
         {Button && <VStack transform={"translateY(100%)"} >{Button}</VStack>}
         </Container>
