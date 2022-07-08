455d1197549a42d5e123cf6a0d5ebb0814955ecb
sora-k46 <sorrawit456789@gmail.com>
2022-07-08 21:16:36 +0700

    Add optional component on the top of container

--- src/components/BoxContainer.tsx
+++ src/components/BoxContainer.tsx
@@ -1,9 +1,10 @@
 import { Box, Button, Container, VStack } from '@chakra-ui/react'
 import { FC, ReactNode } from 'react'
+import MissionBox from './MissionBox'
 
 
 
-const BoxContainer:FC<{children:ReactNode,Button?:ReactNode}> = ({children,Button}) => {
+const BoxContainer:FC<{children:ReactNode,Button?:ReactNode,MissionBox?:ReactNode}> = ({children,Button,MissionBox}) => {
   return (
     <Box border="1px solid"
      borderColor={"white"}
@@ -11,6 +12,7 @@ const BoxContainer:FC<{children:ReactNode,Button?:ReactNode}> = ({children,Butto
       bg="#EBDCFC"
        w="100%" rounded={"3xl"}  display="flex" justifyContent={"center"} p={5}>
       <Container maxW="lg" >
+        {MissionBox && <VStack transform={'translateY(-100%)'} >{MissionBox}</VStack>}
         {children}
         {Button && <VStack transform={"translateY(100%)"} >{Button}</VStack>}
         </Container>
