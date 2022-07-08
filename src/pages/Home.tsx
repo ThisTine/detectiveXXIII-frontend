39b77fcaf8dd9e6be2bb94873eee83d95cd9620c
sora-k46 <sorrawit456789@gmail.com>
2022-07-08 21:19:21 +0700

    Import MissionBox and add as prop to BoxContainer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,13 +5,17 @@ import userContext from '../context/userContext'
 import AppLayout from '../layouts/AppLayout'
 import getImageUrl from '../functions/getImageUrl'
 import PrimaryButton from '../components/PrimaryButton'
+import MissionBox from '../components/MissionBox'
 
 const Home = () => {
   const [mission] = useState<string>("go to Lx Build")
   const { logout, user } = useContext(userContext)
   return (
     <AppLayout nav >
-      <BoxContainer Button={<PrimaryButton onClick={logout} >Logout</PrimaryButton>} >
+      <BoxContainer
+        Button={<PrimaryButton onClick={logout} >Logout</PrimaryButton>}
+        MissionBox={<MissionBox>Mission</MissionBox>}
+      >
         <Box display='flex' flexDirection='column' alignItems='center' >
           <Avatar size={"2xl"} src={getImageUrl(user?.img.data)} />
           <Heading>Home</Heading>
