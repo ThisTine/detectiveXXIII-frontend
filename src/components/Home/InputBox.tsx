76e4e3eb9957ff20ead4492cacd6655e0a18f786
sora-k46 <sorrawit456789@gmail.com>
2022-07-09 12:36:30 +0700

    Seperate toast and clean some code

--- src/components/Home/InputBox.tsx
+++ src/components/Home/InputBox.tsx
@@ -1,6 +1,60 @@
 import { AlertIcon, Alert, Box, FormControl, Input, useToast, AlertTitle, AlertDescription, CloseButton, useDisclosure } from "@chakra-ui/react";
 import { useState, FC } from "react";
 
+const InputBox = () => {
+
+  const [code, setCode] = useState('');
+  // Is the code match with P'รหัส
+  const [isCorrect, setAnswer] = useState(false);
+  const toastCorrect = useToast();
+  const toastWrong = useToast();
+
+  function handleSubmit(e: any) {
+    // checkWithDatabase() return true or false and setAnswer()
+    setAnswer(true);
+    isCorrect ?
+      (toastCorrect({
+        position: 'top',
+        render: () => (
+          <AlertResult answer={true} />
+        ),
+      }))
+      :
+      (toastWrong({
+        position: 'top',
+        render: () => (
+          <AlertResult answer={false} />
+        ),
+      }))
+    setCode('');
+    e.preventDefault();
+  }
+
+  return (
+    <Box>
+      <FormControl as='form' onSubmit={(e) => handleSubmit(e)} borderColor={"#A37BFF"} >
+        <Input
+          id='CODE'
+          width='auto'
+          p={6}
+          bg='whiteAlpha.500'
+          boxShadow='lg'
+          borderRadius={60}
+          placeholder='PUT CODE HERE'
+          textAlign={['center']}
+          _placeholder={{
+            color: '#AD89FF'
+          }}
+          textColor='#AD89FF'
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+        />
+        <button type='submit'>submit ปลอมๆ</button>
+      </FormControl>
+    </Box>
+  );
+}
+
 const AlertResult: FC<{ answer: boolean }> = ({ answer }) => {
   const {
     isOpen: isVisible,
@@ -27,61 +81,6 @@ const AlertResult: FC<{ answer: boolean }> = ({ answer }) => {
         onClick={onClose}
       />
     </Alert >
-  );
-}
-
-const InputBox = () => {
-
-  function handleSubmit(e: any) {
-    // checkWithDatabase() return true or false and setAnswer()
-    setAnswer(true);
-    isCorrect ?
-      (toast({
-        position: 'top',
-        render: () => (
-          <AlertResult answer={true} />
-        ),
-      }))
-      :
-      (toast({
-        position: 'top',
-        render: () => (
-          <AlertResult answer={false} />
-        ),
-      }))
-    setCode('');
-    e.preventDefault();
-  }
-
-  const [code, setCode] = useState('');
-
-  // Is the code match with P'รหัส
-  const [isCorrect, setAnswer] = useState(false);
-
-  const toast = useToast();
-
-  return (
-    <Box>
-      <FormControl as='form' onSubmit={(e) => handleSubmit(e)} borderColor={"#A37BFF"} >
-        <Input
-          id='CODE'
-          width='auto'
-          p={6}
-          bg='whiteAlpha.500'
-          boxShadow='lg'
-          borderRadius={60}
-          placeholder='PUT CODE HERE'
-          textAlign={['center']}
-          _placeholder={{
-            color: '#AD89FF'
-          }}
-          textColor='#AD89FF'
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-        />
-        <button type='submit'>submit ปลอมๆ</button>
-      </FormControl>
-    </Box>
   );
 }
 
