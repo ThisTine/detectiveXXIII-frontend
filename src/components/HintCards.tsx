f2cda1abe3b2178988a44f13eccd249b2ac5378c
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2022-07-09 19:18:49 +0700

    add hint and modal

--- src/components/HintCards.tsx
+++ src/components/HintCards.tsx
@@ -8,33 +8,84 @@ import {
   Badge,
 } from "@chakra-ui/react";
 import { FaLock, FaCheck } from "react-icons/fa";
-function HintCards(props) {
-  return (
-    <Box minW="300px" width={{base:"100%",sm:"80%"}} boxShadow="md" p="4" rounded="lg" bg="white">
-      <Flex
-        alignItems="center"
-        justifyContent="space-between"
-        gap="2"
-      >
-        <Flex
-          gap="5"
-          alignItems="center"
-        >
-          <Box bg="#C6F6D5" p="5" rounded="lg">
+function HintCards({ hints, id }) {
+  if(hints.isShow){
+    return(
+      <Box
+      minW="300px"
+      width={{ base: "50%", sm: "80%" }}
+      boxShadow="md"
+      p="4"
+      rounded="lg"
+      bg="white"
+    >
+      <Flex alignItems="center" justifyContent="space-between" gap="2">
+        <Flex alignItems="center">
+          <Flex
+            bg="#C6F6D5"
+            width="50px"
+            height="50px"
+            rounded="lg"
+            justifyContent="center"
+            alignItems="center"
+            marginRight={3}
+          >
             <Icon as={FaCheck}></Icon>
-          </Box>
-          <Heading as="h3" size="sm">
-            Hint 1
-          </Heading>
-          <UnorderedList>
-            <ListItem>Go to Lx building</ListItem>
-          </UnorderedList>
+          </Flex>
+          <Flex flexDirection="column">
+            <Heading as="h3" size="sm" fontWeight="semibold">
+              HINT {id + 1}
+            </Heading>
+            <UnorderedList>
+              <ListItem color="#4f4f4f">{hints.hint}</ListItem>
+            </UnorderedList>
+          </Flex>
         </Flex>
-        <Badge ml="1" colorScheme="green" variant='outline'>
+        <Badge ml="1" colorScheme="green" variant="outline">
           Unlocked
         </Badge>
       </Flex>
     </Box>
-  );
+    );
+  }
+  else{
+    return(
+      <Box
+      minW="300px"
+      width={{ base: "50%", sm: "80%" }}
+      boxShadow="md"
+      p="4"
+      rounded="lg"
+      bg="white"
+    >
+      <Flex alignItems="center" justifyContent="space-between" gap="2">
+        <Flex alignItems="center">
+          <Flex
+            bg="#EBDCFC"
+            width="50px"
+            height="50px"
+            rounded="lg"
+            justifyContent="center"
+            alignItems="center"
+            marginRight={3}
+          >
+            <Icon as={FaLock}></Icon>
+          </Flex>
+          <Flex flexDirection="column">
+            <Heading as="h3" size="sm" fontWeight="semibold">
+              HINT {id + 1}
+            </Heading>
+            <UnorderedList>
+              <ListItem color="#4f4f4f">{hints.hint}</ListItem>
+            </UnorderedList>
+          </Flex>
+        </Flex>
+        <Badge ml="1" colorScheme="red" variant="outline">
+          Locked
+        </Badge>
+      </Flex>
+    </Box>
+    );
+  }
 }
 export default HintCards;
--- src/pages/Hints.tsx
+++ src/pages/Hints.tsx
@@ -1,17 +1,28 @@
 import HintCards from "../components/HintCards";
 import AppLayout from "../layouts/AppLayout";
+import { FaLock } from "react-icons/fa";
 import { useState } from "react";
-import { Flex } from "@chakra-ui/react";
+import { Button, Icon } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react'
 
 type hints = { userId: 1 | 2; hint: string; isShow: boolean }[];
 type life = 0 | 1 | 2 | 3 | 4 | 5;
 
 const mockhints: hints = [
-  { userId: 1, hint: "ทดสอบ", isShow: true },
-  { userId: 2, hint: "ทดสอบ", isShow: true },
-  { userId: 1, hint: "ทดสอบ", isShow: true },
-  { userId: 2, hint: "ทดสอบ", isShow: true },
-  // { userId: 1, hint: "ทดสอบ", isShow: true },
+  { userId: 1, hint: "Lx building", isShow: true },
+  { userId: 2, hint: "Lx building", isShow: false },
+  { userId: 1, hint: "Lx building", isShow: false },
+  { userId: 2, hint: "Lx building", isShow: false },
+  { userId: 1, hint: "Lx building", isShow: false },
   // { userId: 2, hint: "ทดสอบ", isShow: true },
   // { userId: 1, hint: "ทดสอบ", isShow: true },
   // { userId: 2, hint: "ทดสอบ", isShow: true },
@@ -28,13 +39,43 @@ const mockhints: hints = [
 ];
 
 const Hints = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [hints] = useState<hints>(mockhints);
   const [life] = useState<life>(0);
   return (
-    <AppLayout nav flexDirection="column" gap="5" marginY="5">
-      {mockhints.map((hints,index)=>{
-        return <HintCards key={index} hints={hints} />
+    <AppLayout nav flexDirection="column" gap={{ base: "2", sm: "5" }}>
+      {mockhints.map((hints, index) => {
+        return <HintCards key={index} hints={hints} id={index} />;
       })}
+      <Button
+        bg="#BDA2FF"
+        width="80px"
+        height="80px"
+        textColor="white"
+        rounded="full"
+        _hover={{ textColor: "black" , bg:"lightgrey"}}
+        onClick={onOpen}
+      >
+        <Icon as={FaLock} w={6} h={6} />
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader >Open 1 more hints</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          you are about to spend 3 lifes on a hint. <br />
+          are you sure that you would like to see this hint ?
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variant='ghost' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button  colorScheme='purple'>Yes</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </AppLayout>
   );
 };
