e5f32e93258e30fa9c97492bc3537ea55afdfe32
sora-k46 <sorrawit456789@gmail.com>
2022-07-09 12:20:11 +0700

    Format Code

--- src/components/Home/MissionBox.tsx
+++ src/components/Home/MissionBox.tsx
@@ -4,7 +4,12 @@ import { FC, ReactNode } from 'react'
 const MissionBox: FC<{ children: ReactNode, boxStyle: any }> = ({ children, boxStyle }) => {
 
   return (
-    <Box {...boxStyle} transform={"translateY(50%)"} display='flex' justifyContent='flex-start' >
+    <Box
+      {...boxStyle}
+      transform={"translateY(50%)"}
+      display='flex'
+      justifyContent='flex-start'
+    >
       <Box backgroundColor='#EBDCFC' rounded='lg' width='50px' height='50px' ></Box>
       <VStack ml="6">
         <Heading as='h5' size="sm">New Mission</Heading>
