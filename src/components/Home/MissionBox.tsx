49f20ed6f26061b547baa21ec4e5e4500ddf8479
sora-k46 <sorrawit456789@gmail.com>
2022-07-10 10:37:45 +0700

    Add react-icon and format code

--- src/components/Home/MissionBox.tsx
+++ src/components/Home/MissionBox.tsx
@@ -1,5 +1,6 @@
 import { Text, Heading, VStack, Box, BoxProps } from '@chakra-ui/react';
 import { FC, ReactNode } from 'react'
+import { TbZoomQuestion } from 'react-icons/tb'
 
 
 const MissionBox: FC<{ children: ReactNode, boxStyle: BoxProps }> = ({ children, boxStyle }) => {
@@ -11,7 +12,17 @@ const MissionBox: FC<{ children: ReactNode, boxStyle: BoxProps }> = ({ children,
       display='flex'
       justifyContent='flex-start'
     >
-      <Box backgroundColor='#EBDCFC' rounded='lg' width='50px' height='50px' ></Box>
+      <Box
+        backgroundColor='#EBDCFC'
+        rounded='lg'
+        width='50px'
+        height='50px'
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <TbZoomQuestion color='#A680FF' size='30px' />
+      </Box>
       <VStack ml="6">
         <Heading as='h5' size="sm">New Mission</Heading>
         <Text>
