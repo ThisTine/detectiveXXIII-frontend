e37736f46fe3de566b574bed840d1b33b4d4c3b9
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2022-07-10 15:14:25 +0700

    edit: if-else inline

--- src/components/HintCards.tsx
+++ src/components/HintCards.tsx
@@ -9,7 +9,6 @@ import {
 } from "@chakra-ui/react";
 import { FaLock, FaCheck } from "react-icons/fa";
 function HintCards({hints }:any) {
-  if(hints.isShow){
     return(
       <Box
       minW="300px"
@@ -22,7 +21,7 @@ function HintCards({hints }:any) {
       <Flex alignItems="center" justifyContent="space-between" gap="2">
         <Flex alignItems="center">
           <Flex
-            bg="#C6F6D5"
+            bg={hints.isShow ? "green.100" : "red.100"}
             width="50px"
             height="50px"
             rounded="lg"
@@ -30,7 +29,7 @@ function HintCards({hints }:any) {
             alignItems="center"
             marginRight={3}
           >
-            <Icon as={FaCheck}></Icon>
+            <Icon as={hints.isShow ? FaCheck : FaLock} />
           </Flex>
           <Flex flexDirection="column">
             <Heading as="h3" size="sm" fontWeight="semibold">
@@ -41,52 +40,50 @@ function HintCards({hints }:any) {
             </UnorderedList>
           </Flex>
         </Flex>
-        <Badge ml="1" colorScheme="green" variant="outline">
+        <Badge ml="1" colorScheme={hints.isShow ? "green" : "red"} variant="outline">
           Unlocked
         </Badge>
       </Flex>
     </Box>
     );
   }
-  //is Show is False
-  else{
-    return(
-      <Box
-      minW="300px"
-      width={{ base: "50%", sm: "80%" }}
-      boxShadow="md"
-      p="4"
-      rounded="lg"
-      bg="white"
-    >
-      <Flex alignItems="center" justifyContent="space-between" gap="2">
-        <Flex alignItems="center">
-          <Flex
-            bg="#EBDCFC"
-            width="50px"
-            height="50px"
-            rounded="lg"
-            justifyContent="center"
-            alignItems="center"
-            marginRight={3}
-          >
-            <Icon as={FaLock}></Icon>
-          </Flex>
-          <Flex flexDirection="column">
-            <Heading as="h3" size="sm" fontWeight="semibold">
-              HINT {hints.id}
-            </Heading>
-            <UnorderedList>
-              <ListItem color="#4f4f4f" filter="auto" blur="10px">{hints.hint}</ListItem>
-            </UnorderedList>
-          </Flex>
-        </Flex>
-        <Badge ml="1" colorScheme="red" variant="outline">
-          Locked
-        </Badge>
-      </Flex>
-    </Box>
-    );
-  }
-}
+  // else{
+  //   return(
+  //     <Box
+  //     minW="300px"
+  //     width={{ base: "50%", sm: "80%" }}
+  //     boxShadow="md"
+  //     p="4"
+  //     rounded="lg"
+  //     bg="white"
+  //   >
+  //     <Flex alignItems="center" justifyContent="space-between" gap="2">
+  //       <Flex alignItems="center">
+  //         <Flex
+  //           bg="#EBDCFC"
+  //           width="50px"
+  //           height="50px"
+  //           rounded="lg"
+  //           justifyContent="center"
+  //           alignItems="center"
+  //           marginRight={3}
+  //         >
+  //           <Icon as={FaLock}></Icon>
+  //         </Flex>
+  //         <Flex flexDirection="column">
+  //           <Heading as="h3" size="sm" fontWeight="semibold">
+  //             HINT {hints.id}
+  //           </Heading>
+  //           <UnorderedList>
+  //             <ListItem color="#4f4f4f" filter="auto" blur="10px">{hints.hint}</ListItem>
+  //           </UnorderedList>
+  //         </Flex>
+  //       </Flex>
+  //       <Badge ml="1" colorScheme="red" variant="outline">
+  //         Locked
+  //       </Badge>
+  //     </Flex>
+  //   </Box>
+  //   );
+  // }
 export default HintCards;
