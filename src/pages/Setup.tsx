4b360b6fa56a65f9f329c4b362c6690c3e17a121
fah <phurichayakhem1@gmail.com>
2022-07-10 14:02:01 +0700

    hint error checking

--- src/pages/Setup.tsx
+++ src/pages/Setup.tsx
@@ -21,8 +21,35 @@ const Setup = () => {
   const InputChange = (e: {
     target: { value: React.SetStateAction<string> };
   }) => setInput(e.target.value);
-  const isError = input.length > 10;
+  const isErrorByL = input.length > 10;
+  const isErrorByrepeat = hints.includes(input);
+  const isErrorByEmpty = input.length === 0;
 
+  const submit = () => {
+    if (checkError()) {
+      toast({
+        position: "top",
+        title: "Fail",
+        status: "error",
+        variant: "left-accent",
+        isClosable: true,
+        description: checkError(),
+      });
+    } else {
+      console.log(hints);
+    }
+  };
+  const checkError = () => {
+    if (isErrorByL) {
+      return "WE ONELY ALLOW 10 LETTERS FOR HINTS !";
+    } else if (isErrorByrepeat) {
+      return "Your hint is already in the list.";
+    } else if (isErrorByEmpty) {
+      return "Put your hint before submit.";
+    } else {
+      return "";
+    }
+  };
   const toast = useToast();
   return (
     <AppLayout nav flexDirection={"column"} maxW="md">
@@ -32,18 +59,7 @@ const Setup = () => {
 
       <BoxContainer
         Button={
-          <PrimaryButton
-            onClick={() =>
-              toast({
-                position: "top",
-                title: "Fail",
-                status: "error",
-                variant: "left-accent",
-                isClosable: true,
-                description: "You just added this hint !",
-              })
-            }
-          >
+          <PrimaryButton onClick={submit}>
             <HStack>
               <Text>NEXT </Text>
               <AiOutlineArrowRight />
@@ -56,7 +72,7 @@ const Setup = () => {
             ADD HINT
           </Text>
           <Center h={150} textAlign="center">
-            <FormControl isInvalid={isError}>
+            <FormControl isInvalid={isErrorByL || isErrorByrepeat}>
               <Input
                 borderColor="#A680FF"
                 focusBorderColor="#A680FF"
@@ -73,10 +89,18 @@ const Setup = () => {
                 id="hint"
                 value={input}
                 onChange={InputChange}
-                isInvalid={isError}
+                isInvalid={isErrorByL || isErrorByrepeat}
               />
-              <FormErrorMessage color="#FF0000" fontWeight="600" fontSize="16">
-                WE ONELY ALLOW 10 LETTERS FOR HINTS.
+
+              <FormErrorMessage
+                color="#FF0000"
+                fontWeight="600"
+                fontSize="16"
+                width="100%"
+              >
+                <Text textAlign="center" width="100%">
+                  {checkError()}
+                </Text>
               </FormErrorMessage>
             </FormControl>
           </Center>
