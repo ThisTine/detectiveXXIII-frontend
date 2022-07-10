c2a5dec331146f115e27cfcf2483d3890fb0ae0c
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2022-07-10 15:17:20 +0700

    edit: inline if else 44

--- src/components/HintCards.tsx
+++ src/components/HintCards.tsx
@@ -41,49 +41,10 @@ function HintCards({hints }:any) {
           </Flex>
         </Flex>
         <Badge ml="1" colorScheme={hints.isShow ? "green" : "red"} variant="outline">
-          Unlocked
+          {hints.isShow ? "Unlocked" : "Locked"}
         </Badge>
       </Flex>
     </Box>
     );
   }
-  // else{
-  //   return(
-  //     <Box
-  //     minW="300px"
-  //     width={{ base: "50%", sm: "80%" }}
-  //     boxShadow="md"
-  //     p="4"
-  //     rounded="lg"
-  //     bg="white"
-  //   >
-  //     <Flex alignItems="center" justifyContent="space-between" gap="2">
-  //       <Flex alignItems="center">
-  //         <Flex
-  //           bg="#EBDCFC"
-  //           width="50px"
-  //           height="50px"
-  //           rounded="lg"
-  //           justifyContent="center"
-  //           alignItems="center"
-  //           marginRight={3}
-  //         >
-  //           <Icon as={FaLock}></Icon>
-  //         </Flex>
-  //         <Flex flexDirection="column">
-  //           <Heading as="h3" size="sm" fontWeight="semibold">
-  //             HINT {hints.id}
-  //           </Heading>
-  //           <UnorderedList>
-  //             <ListItem color="#4f4f4f" filter="auto" blur="10px">{hints.hint}</ListItem>
-  //           </UnorderedList>
-  //         </Flex>
-  //       </Flex>
-  //       <Badge ml="1" colorScheme="red" variant="outline">
-  //         Locked
-  //       </Badge>
-  //     </Flex>
-  //   </Box>
-  //   );
-  // }
 export default HintCards;
