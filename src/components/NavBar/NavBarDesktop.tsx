0051599058616c1ea426ad18c083b84b274fd7ac
I_AM_TINE <sutichok46@gmail.com>
2022-07-24 23:34:07 +0700

    add logout button

--- src/components/NavBar/NavBarDesktop.tsx
+++ src/components/NavBar/NavBarDesktop.tsx
@@ -1,8 +1,9 @@
-import {  Container, Heading, HStack, Tooltip, VStack } from '@chakra-ui/react'
-import { FC, ReactNode } from 'react'
+import {  Button, Container, Heading, HStack, Tooltip, VStack } from '@chakra-ui/react'
+import { FC, ReactNode, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import NavBarData from './NavBarData'
-import {AiFillHeart} from 'react-icons/ai'
+import {AiFillHeart, AiOutlineLogout} from 'react-icons/ai'
+import userContext from '../../context/userContext'
 
 const ButtonIcon:FC<{children:ReactNode,to:string,title:string}> = ({children,to,title})=>{
   return <Link {...{to}} ><Tooltip label={title} aria-label={title} ><Heading color="#A680FF" cursor={"pointer"} transition="0.25s" _hover={{color:"#9466FF"}} size={"xl"} >{children}</Heading>
@@ -10,6 +11,7 @@ const ButtonIcon:FC<{children:ReactNode,to:string,title:string}> = ({children,to
 }
 
 const NavBarDesktop:FC<{isMobile:boolean}> = ({isMobile}) => {
+  const {logout} = useContext(userContext)
   return (
     <HStack w="100%" pos="fixed" top={0} justifyContent="flex-end" >
       <Container w="100%" maxW={"container.xl"} pt={10} >
@@ -17,7 +19,11 @@ const NavBarDesktop:FC<{isMobile:boolean}> = ({isMobile}) => {
       {!isMobile && <HStack  gap={8}> 
       {NavBarData.map(({Component,to,title})=><ButtonIcon to={to} title={title} key={title} > <Component/>  </ButtonIcon>)}
         </HStack>}
+        <HStack>
         <HStack bg="white" gap={2} p={2} px={3} shadow="lg" rounded="2xl"><Heading size={"lg"} color="#F56565" ><AiFillHeart/></Heading><Heading size={"lg"} color="#A680FF" >5</Heading></HStack>
+        <Button w="100%" h="100%" bg="white" onClick={logout} gap={2} p={2} px={3} shadow="lg" rounded="2xl"> <Tooltip label="Logout"><Heading size={"lg"} color="#F56565" > <AiOutlineLogout/> </Heading></Tooltip> </Button>
+      
+        </HStack>
         </VStack>
 
       </Container>
