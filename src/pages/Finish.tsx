524f567808eb8230c9cf5b89d9301edf23f4577e
Thun (Mac Swift 3) <bsthun@gmail.com>
2022-07-31 11:04:09 +0700

    Fix reversion

--- src/pages/Finish.tsx
+++ src/pages/Finish.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
 import { Avatar, Box, Flex, Text, useBreakpointValue } from "@chakra-ui/react"
+import React, { useContext, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import BoxContainer from "../components/BoxContainer"
 import PrimaryButton from "../components/PrimaryButton"
 import userContext from "../context/userContext"
@@ -9,81 +9,16 @@ import AppLayout from "../layouts/AppLayout"
 
 type User = { name: string; img: { type: "Buffer"; data: number[] } | null }
 
-type CarouselCompProp = {
-    users: User[]
-}
-
-const CarouselComp = (props: CarouselCompProp) => {
-    const [render, setRender] = useState(true)
-
-    useEffect(() => {
-        setRender(false)
-    }, [props])
-
-    useEffect(() => {
-        setRender(true)
-    }, [!render])
-
-    return render ? (
-        <Carousel
-            show={1}
-            slide={1}
-            dynamic={true}
-            swiping={true}
-            swipeOn={0.5}
-            leftArrow={
-                <Flex height="calc(100% - 90px)" alignItems="center" position="relative">
-                    <Box rounded="full" bgColor="white" shadow="xl" position="absolute" left={-4} zIndex={100} cursor="pointer">
-                        <ChevronLeftIcon fontSize="4xl" />
-                    </Box>
-                </Flex>
-            }
-            rightArrow={
-                <Flex height="calc(100% - 90px)" alignItems="center" position="relative">
-                    <Box rounded="full" bgColor="white" shadow="xl" position="absolute" right={-4} zIndex={100} cursor="pointer">
-                        <ChevronRightIcon fontSize="4xl" />
-                    </Box>
-                </Flex>
-            }
-        >
-            {props.users.map((user, index) => (
-                <Flex key={index} padding={5} direction="column" alignItems="center" gap={4}>
-                    <Avatar
-                        width="210"
-                        height="210"
-                        name={user.name}
-                        src={getImageUrl(user.img?.data)}
-                        boxShadow="0px 4px 15px rgba(0, 0, 0, 0.25)"
-                    />
-                    <Box
-                        backgroundColor="#FFFFFF"
-                        border="0.5px solid rgba(0, 0, 0, 0.23)"
-                        boxShadow="0px 4px 15px rgba(0, 0, 0, 0.25)"
-                        borderRadius="full"
-                        py="3"
-                        px="6"
-                    >
-                        <Text textAlign="center" color="mainBtn.700" fontWeight={500} fontSize="17px" textTransform="uppercase">
-                            {user.name}
-                        </Text>
-                    </Box>
-                </Flex>
-            ))}
-        </Carousel>
-    ) : (
-        <></>
-    )
-}
-
 const Finish = () => {
-    const avatarSize = useBreakpointValue({ base: "190px", md: "220px" })
-    const avatarInset = useBreakpointValue({ base: "90px", md: "120px" })
-
     const {
         partner: { partners },
         user: { partnerCount },
     } = useContext(userContext)
     const navigate = useNavigate()
+
+    const avatarSize = useBreakpointValue({ base: "190px", md: "220px" })
+    const avatarInset = useBreakpointValue({ base: "90px", md: "120px" })
+
     useEffect(() => {
         if (!partners.length) {
             navigate("/", { replace: true })
@@ -108,7 +43,7 @@ const Finish = () => {
                     <Text fontSize="xl" color="mainBtn.700">
                         Your match is:
                     </Text>
-                    {partners.user.length > 1 ? (
+                    {partners.length > 1 ? (
                         <Box position="relative" width="100%" maxWidth="390px" minWidth="350px" height={`calc(${avatarSize!} + ${avatarInset!})`}>
                             <Avatar
                                 position="absolute"
@@ -116,8 +51,8 @@ const Finish = () => {
                                 left="10px"
                                 width={avatarSize}
                                 height={avatarSize}
-                                name={partners.user[0].name}
-                                src={getImageUrl(partners.user[0].img.data)}
+                                name={partners[0].name}
+                                src={getImageUrl(partners[0].img?.data)}
                                 boxShadow="0px 4px 15px rgba(0, 0, 0, 0.25)"
                             />
                             <Avatar
@@ -126,8 +61,8 @@ const Finish = () => {
                                 right="10px"
                                 width={avatarSize}
                                 height={avatarSize}
-                                name={partners.user[0].name}
-                                src={getImageUrl(partners.user[1].img.data)}
+                                name={partners[0].name}
+                                src={getImageUrl(partners[1].img?.data)}
                                 boxShadow="0px 4px 15px rgba(0, 0, 0, 0.25)"
                             />
                         </Box>
@@ -135,13 +70,13 @@ const Finish = () => {
                         <Avatar
                             width="210"
                             height="210"
-                            name={partners.user[0].name}
-                            src={getImageUrl(partners.user[0].img.data)}
+                            name={partners[0].name}
+                            src={getImageUrl(partners[0].img?.data)}
                             boxShadow="0px 4px 15px rgba(0, 0, 0, 0.25)"
                         />
                     )}
                     <Flex direction={["column", "row"]} gap={2}>
-                        {partners.user.map((user, index) => (
+                        {partners.map((user, index) => (
                             <Box
                                 backgroundColor="#FFFFFF"
                                 border="0.5px solid rgba(0, 0, 0, 0.23)"
